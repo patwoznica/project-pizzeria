13fbad7e4e96e3ccd22a64ae5298d1b06f786375
Patryk Woznica <pat.woznica@gmail.com>
2021-09-03 18:56:42 +0200

    Module 9.3 importowanie zmian w plikach

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,10 +2,13 @@ export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product', // CODE ADDED
+    bookingWidget: '#template-booking-widget',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -25,6 +28,15 @@ export const select = {
       input: 'input.amount', // CODE CHANGED
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
+    },
+	  datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+	  hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
     },
   },
   // CODE ADDED START
@@ -47,6 +59,9 @@ export const select = {
     remove: '[href="#remove"]',
   },
   // CODE ADDED END
+  nav: {
+    links: '.main-nav a',
+  },
 };
 
 export const classNames = {
@@ -59,6 +74,18 @@ export const classNames = {
     wrapperActive: 'active',
   },
   // CODE ADDED END
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+    tableSelected: 'selected',
+    table: 'table',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  }
 };
 
 export const settings = {
@@ -76,6 +103,24 @@ export const settings = {
     url: '//localhost:3131',
     products: 'products',
     orders: 'orders',
+    product: 'product',
+    order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+  },
+  hours: {
+    open: 12,
+    close: 24,
+  },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+  booking:{
+    tableIdAttribute: 'data-table',
   },
 };
 
@@ -84,4 +129,5 @@ export const templates = {
   // CODE ADDED START
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   // CODE ADDED END
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
